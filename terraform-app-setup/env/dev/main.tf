b16b52383632d64f7a93188a81fded12c375fd4d
palak53 <palmejpara53@gmail.com>
2025-08-27 16:01:49 -0400

    commit 3 : adding 3 - private + public subnet accross Azs

--- terraform-app-setup/env/dev/main.tf
+++ terraform-app-setup/env/dev/main.tf
@@ -5,7 +5,7 @@ module "network_vpc" {
 }
 
 module "network_subnet" {
-  source               = "../../modules/network/subnet"
+  source               = "../../modules/network/subnet" 
   vpc_id               = module.network_vpc.vpc_id
   azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
